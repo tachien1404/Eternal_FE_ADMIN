19ebebad45b704ff7a8220fbdaf6609ea92e99ea
Nguyễn Quang Dương <102996301+duong13012002@users.noreply.github.com>
2022-10-19 23:35:55 +0700

    Front Admin

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,24 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import {AdminLayoutComponent} from "./layouts/admin-layout/admin-layout.component";
 
-const routes: Routes = [];
 
-@NgModule({
-  imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
-})
-export class AppRoutingModule { }
+
+export const AppRoutingModule:  Routes = [
+  {
+    path: '',
+    redirectTo: 'dashboard',
+    pathMatch: 'full',
+  }, {
+    path: '',
+    component: AdminLayoutComponent,
+    children: [
+      {
+        path: '',
+        loadChildren: () => import('./layouts/admin-layout/admin-layout.module').then(x => x.AdminLayoutModule)
+      }]},
+  {
+    path: '**',
+    redirectTo: 'dashboard'
+  }
+]
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,25 @@
+import { AdminLayoutComponent } from './layouts/admin-layout/admin-layout.component';
+import { SidebarModule } from './sidebar/sidebar.module';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import {SidebarComponent} from "./sidebar/sidebar.component";
+import { RouterModule } from '@angular/router';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AdminLayoutComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    SidebarModule,
+    RouterModule.forRoot(AppRoutingModule,{
+      useHash: true
+    })
   ],
   providers: [],
   bootstrap: [AppComponent]
