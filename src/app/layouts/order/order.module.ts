44bfc05dfad4d4ba688fb9f880ea1d4b2d2baaf0
TaQuangTuan02 <101920185+TaQuangTuan02@users.noreply.github.com>
2023-01-29 20:32:41 +0700

    chọn mau laod sai , bán qua giao hàng

--- src/app/layouts/order/order.module.ts
+++ src/app/layouts/order/order.module.ts
@@ -1,30 +1,19 @@
 import {NgModule} from "@angular/core";
 import {CommonModule} from "@angular/common";
 import {OrderRouting} from "./order.routing";
-import { TatcaComponent } from './tatca/tatca.component';
-import { ChoxacnhanComponent } from './choxacnhan/choxacnhan.component';
-import { DahuyComponent } from './dahuy/dahuy.component';
-import { DanggiaoComponent } from './danggiao/danggiao.component';
-import { DaGiaoComponent } from './da-giao/da-giao.component';
-import {BrowserAnimationsModule} from "@angular/platform-browser/animations";
+
 import {Material_module} from "../../../material_module";
 
 import {FormsModule, ReactiveFormsModule} from "@angular/forms";
 import {NgxPaginationModule} from "ngx-pagination";
 import {OrderdetailComponent} from "./orderdetail/orderdetail.component";
-import { ChuanbihangComponent } from './chuanbihang/chuanbihang.component';
-import { DonOffComponent } from './don-off/don-off.component';
+
 
 @NgModule({
   declarations:[
-    TatcaComponent,
-    ChoxacnhanComponent,
-    DahuyComponent,
-    DanggiaoComponent,
-    DaGiaoComponent,
+
 OrderdetailComponent,
-ChuanbihangComponent,
-DonOffComponent
+
   ],
     imports: [
 
--- src/app/layouts/order/order.routing.ts
+++ src/app/layouts/order/order.routing.ts
@@ -1,26 +1,13 @@
 import {NgModule} from "@angular/core";
 import {RouterModule, Routes} from "@angular/router";
 import {OrderComponent} from "./order.component";
-import {TatcaComponent} from "./tatca/tatca.component";
-import {ChoxacnhanComponent} from "./choxacnhan/choxacnhan.component";
-import {DahuyComponent} from "./dahuy/dahuy.component";
-import {DanggiaoComponent} from "./danggiao/danggiao.component";
-import {DaGiaoComponent} from "./da-giao/da-giao.component";
 
 import {OrderdetailComponent} from "./orderdetail/orderdetail.component";
-import {ChuanbihangComponent} from "./chuanbihang/chuanbihang.component";
-import {DonOffComponent} from "./don-off/don-off.component";
+
 
 
 const routes: Routes = [{
   path: '', component: OrderComponent, children: [
-    {path: 'tatca', component: TatcaComponent},
-    {path: 'choxacnhan', component: ChoxacnhanComponent},
-    {path: 'dahuy', component: DahuyComponent},
-    {path: 'danggiao', component: DanggiaoComponent},
-    {path: 'dagiao', component: DaGiaoComponent},
-    {path: 'chuanbihang', component: ChuanbihangComponent},
-    {path: 'donoff', component: DonOffComponent},
 
   ]
 
